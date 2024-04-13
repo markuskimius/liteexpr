a4652c95d0501b1c271d7bb7e339ecb1c8c75c2e
Mark Kim <markuskimius@gmail.com>
2024-04-13 10:27:44 -0400

    fix pip install circular dependency

--- python/setup.py
+++ python/setup.py
@@ -7,17 +7,15 @@ from setuptools import setup
 SCRIPTDIR = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, os.path.abspath(os.path.join(SCRIPTDIR, "src")))
 
-import liteexpr
-
 with open(os.path.join(SCRIPTDIR, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
                              name = "liteexpr",
                       description = "A light, expression language.",
-                          version = liteexpr.__version__,
-                          license = liteexpr.__license__,
-                           author = liteexpr.__author__,
+                          version = "0.0.10",
+                          license = "Apache 2.0",
+                           author = "Mark Kim",
                      author_email = "markuskimius+py@gmail.com",
                               url = "https://github.com/markuskimius/liteexpr",
                          keywords = [ "expression", "language", "antlr4" ],
